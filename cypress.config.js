fc7a3e251686a7264342f629a0f75f7dd0df3d8c
Zahangir <alamzahangir365@gmail.com>
2025-10-29 03:45:17 +0600

    test case run on all suits

--- cypress.config.js
+++ cypress.config.js
@@ -1,36 +1,19 @@
-// const { defineConfig } = require("cypress");
-
-// module.exports = defineConfig({
-//   //retries: 1,
-//   screenshot:true,
-//   video:true,
-//   videoCompression: true,
-  
-//   reporter: 'cypress-mochawesome-reporter',
-
-//   e2e: {
-//     setupNodeEvents(on, config) {
-
-//     },
-//   },
-
-//   env: {
-//     loginUrl: "https://magnethub.selisestage.com"
-//   }
-
-// });
-
 
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  screenshot:true,
+  video:true,
+  videoCompression: true,
+  reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
     },
   },
-  
+
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'cypress/results',
